7c4f5489a2cf17c74f879b0624c1186522132f7b
Wesley Francescon <wfrancescons@gmail.com>
2025-08-08 23:48:20 -0300

    refactor: add Header and Footer components with social media icons + new Ko-fi button

---
+++ components/Footer.tsx
@@ -0,0 +1,59 @@
+import GithubIcon from "./icons/GithubIcon.tsx";
+import TelegramIcon from "./icons/TelegramIcon.tsx";
+
+export default function Footer() {
+  return (
+    <footer className="footer md:footer-horizontal bg-base-300 text-neutral-content items-center py-2 md:py-4">
+      <div className="w-full max-w-3xl mx-auto p-4 flex flex-col md:flex-row items-center justify-between gap-4">
+        <aside className="flex flex-col items-center md:items-start gap-1 text-base-content">
+          <span>
+            © {new Date().getFullYear()} -{" "}
+            <a
+              className="link link-hover font-semibold p-0 m-0"
+              href="https://bento.me/wfrancescons"
+              target="_blank"
+              rel="noopener"
+            >
+              Wesley Francescon
+            </a>
+          </span>
+          <span className="text-xs text-base-content/60">
+            This project is not associated with Letterboxd.
+          </span>
+        </aside>
+        <nav className="flex gap-4 items-center">
+          <a
+            href="https://ko-fi.com/wfrancescons"
+            target="_blank"
+            rel="noopener"
+            className="btn btn-soft btn-sm"
+          >
+            <img
+              src="https://storage.ko-fi.com/cdn/cup-border.png"
+              alt="Ko-fi"
+              className="inline-block h-3 w-4"
+            />
+            Support me on Ko-fi
+          </a>
+          <div className="divider divider-horizontal m-0" />
+          <div className="grid grid-flow-col gap-4">
+            <a
+              href="https://t.me/letterboxdgrambot"
+              target="_blank"
+              rel="noopener"
+            >
+              <TelegramIcon className="fill-current w-6 h-6" />
+            </a>
+            <a
+              href="https://github.com/wfrancescons"
+              target="_blank"
+              rel="noopener"
+            >
+              <GithubIcon className="fill-current w-6 h-6" />
+            </a>
+          </div>
+        </nav>
+      </div>
+    </footer>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+export default function Header() {
+  return (
+    <header>
+      <nav className="navbar flex-none w-full justify-center items-center">
+        <div className="navbar-center max-w-6xl flex items-center pt-5 pb-2">
+          <img
+            src="/boxdgrid-logo.png"
+            alt="BoxdGrid Logo"
+            className="w-full max-w-40"
+          />
+        </div>
+      </nav>
+
+      <div className="text-sm px-20 flex justify-center items-center text-center">
+        Create grid-style collages of your latest Letterboxd watches
+      </div>
+    </header>
+  );
+}
