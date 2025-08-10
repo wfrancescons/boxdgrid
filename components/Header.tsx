e0cdf1534412cf74eb689d1eb07b52894d8ba6dd
Wesley Francescon <wfrancescons@gmail.com>
2025-08-09 22:13:55 -0300

    refactor: replace PNG with WEBP format for improved performance

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
       <nav className="navbar flex-none w-full justify-center items-center">
         <div className="navbar-center max-w-6xl flex items-center pt-5 pb-2">
           <img
-            src="/boxdgrid-logo.png"
+            src="/boxdgrid-logo.webp"
             alt="BoxdGrid Logo"
             className="w-full max-w-40"
           />
