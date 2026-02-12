10df44495c1e931f00f3137af5b8613feb3c8708
Wesley Francescon <wfrancescons@gmail.com>
2026-02-12 17:17:49 -0300

    build: prevent loading all lucide icons in frontend

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import { fresh } from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import lucidePreprocess from "vite-plugin-lucide-preprocess";
 
 export default defineConfig({
-  plugins: [fresh(), tailwindcss()],
+  plugins: [
+    lucidePreprocess(),
+    fresh(),
+    tailwindcss(),
+    ,
+  ],
   ssr: {
     external: [
       "cheerio",
       "daisyui",
-      "lucide-preact",
     ],
   },
 });
