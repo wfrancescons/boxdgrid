5ea6a3013738b54e7cbf62c0185298c979859353
Wesley Francescon <wfrancescons@gmail.com>
2026-02-13 17:22:29 -0300

    build: add lucidePreprocess plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { fresh } from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-// import lucidePreprocess from "vite-plugin-lucide-preprocess";
+import lucidePreprocess from "vite-plugin-lucide-preprocess";
 
 export default defineConfig({
   plugins: [
-    // lucidePreprocess(),
+    lucidePreprocess(),
     fresh(),
     tailwindcss(),
     ,
