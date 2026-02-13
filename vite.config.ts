d01dd7b03daca9ff03ff731d0a4181404dc286ef
Wesley Francescon <wfrancescons@gmail.com>
2026-02-13 10:55:12 -0300

    ci: try to fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
 import { fresh } from "@fresh/plugin-vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import lucidePreprocess from "vite-plugin-lucide-preprocess";
+// import lucidePreprocess from "vite-plugin-lucide-preprocess";
 
 export default defineConfig({
   plugins: [
-    lucidePreprocess(),
+    // lucidePreprocess(),
     fresh(),
     tailwindcss(),
     ,
   ],
   ssr: {
     external: [
-      "cheerio",
       "daisyui",
     ],
   },
