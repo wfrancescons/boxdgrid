8e65206e2169070c8a155223fb10c6fe742ce18b
Wesley Francescon <wfrancescons@gmail.com>
2026-03-02 23:08:22 -0300

    fix: prevent 3d effect overflow on desktop

--- components/figures/ResultFigure.tsx
+++ components/figures/ResultFigure.tsx
@@ -4,12 +4,12 @@ interface ResultFigureProps {
 
 export default function ResultFigure({ src }: ResultFigureProps) {
   return (
-    <div className="md:hover-3d size-fit max-h-full rounded-md animate-in fade-in zoom-in-50 duration-500">
-      <figure className="w-full h-full rounded-md overflow-hidden">
+    <div className="md:hover-3d w-fit h-full rounded-md animate-in fade-in zoom-in-50 duration-500">
+      <figure className="rounded-md overflow-hidden">
         <img
           src={src}
           alt="Letterboxd collage"
-          className="w-full h-full max-h-[60vh] object-contain"
+          className="max-h-full max-w-full h-auto w-auto object-contain"
         />
       </figure>
 
