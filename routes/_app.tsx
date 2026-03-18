54ce01cfe3bc422096957fac0db94e361bec9180
Wesley Francescon <wfrancescons@gmail.com>
2026-03-17 23:30:20 -0300

    chore: update favicon and add missing platform icons

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,9 +4,28 @@ export default define.page(function App({ Component }) {
   return (
     <html lang="en" translate="no">
       <head>
-        <link href="favicon.ico" rel="icon" sizes="48x48" />
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="favicon.svg"
+        />
+        <link rel="shortcut icon" href="favicon.ico" />
+        <link href="favicon.ico" rel="icon" sizes="48x48" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="boxdgrid" />
+        <link rel="manifest" href="site.webmanifest" />
         <meta name="google" content="notranslate" />
         <title>boxdgrid – Letterboxd Collage Generator</title>
         <link rel="canonical" href="https://boxdgrid.com/" />
